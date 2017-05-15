e0cddb61d776ec7a73561db7af3551bf4f135e39
hudhaifa <hudhaifa@hudhaifa-pc>
2017-05-15 17:32:01 -0500

    - Fixed the watermark

--- code/model/Collectable.php
+++ code/model/Collectable.php
@@ -179,7 +179,7 @@ class Collectable
     /// Single Data Object ///
 
     public function getObjectImage() {
-        return $this->FrontImage()->Watermark();
+        return $this->FrontImage();
     }
 
     public function getObjectDefaultImage() {

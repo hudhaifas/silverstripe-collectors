5f78bc43def7545e969f6b34bf5cc65f7eaf5f57
Hudhaifa Shatnawi <hudhaifa.shatnawi@gmail.com>
2018-04-26 23:09:57 -0500

    - Upgrading to SS4 & BS4

--- src/View/CollectablesPage.php
+++ src/View/CollectablesPage.php
@@ -1,29 +1,5 @@
 <?php
 
-/*
- * MIT License
- *  
- * Copyright (c) 2016 Hudhaifa Shatnawi
- * 
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- * 
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- * 
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- */
-
 /**
  *
  * @author Hudhaifa Shatnawi <hudhaifa.shatnawi@gmail.com>
@@ -87,38 +63,6 @@ class CollectablesPage
         }
 
         return $group;
-    }
-
-}
-
-class CollectablesPage_Controller
-        extends DataObjectPage_Controller {
-
-    protected function getObjectsList() {
-        return DataObject::get($this->Collection)
-                        ->filterByCallback(function($record) {
-                            return $record->canView();
-                        });
-    }
-
-    protected function searchObjects($list, $keywords) {
-        return $list->filterAny(array(
-                    'Title:PartialMatch' => $keywords,
-                    'Summary:PartialMatch' => $keywords,
-                    'Description:PartialMatch' => $keywords,
-                    'Country:PartialMatch' => $keywords,
-                    'Year:PartialMatch' => $keywords,
-                    'SerialNumber:PartialMatch' => $keywords,
-                    'Collector:PartialMatch' => $keywords,
-        ));
-    }
-
-    protected function getFiltersList() {
-        return null;
-    }
-
-    protected function getPageLength() {
-        return 24;
     }
 
 }
